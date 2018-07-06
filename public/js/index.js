f17f7e4363745b4e87f4de30ca2351cef664b8bc
nn <553783595@qq.com>
2018-07-06 14:58:54 +0800

    love

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+var L_l_g={};
+L_l_g.Init=function(){
+    var h = document.documentElement.clientHeight || document.body.clientHeight;
+    console.log(h)
+    // $("body")[0].style.height=document.documentElement.clientHeight+"px" || document.body.clientHeight +"px";
+    writeContent(true);
+}();
