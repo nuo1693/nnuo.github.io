02462b5cca0454be7e5a3e0e449d3bb3aa0320ef
277382422@qq.com <entrance1>
2018-07-09 00:13:45 +0800

    love

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 var L_l_g={};
 L_l_g.Init=function(){
     document.getElementsByTagName("body")[0].style.height=document.documentElement.clientHeight+"px" || document.body.clientHeight +"px";//设置屏幕宽度
-    document.getElementsByTagName("audio")[0].play();//背景音乐播放
+    // document.getElementById("bgMusic").play();//背景音乐播放
     writeContent(true);
 }();
